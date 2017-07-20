7c94f8c51c2cb5fef5ec473f5993c8b37dd31a12
longvdph04236 <longvdph04236@fpt.edu.vn>
2017-07-20 14:01:55 +0700

    t

--- frontend/views/layouts/homepage.php
+++ frontend/views/layouts/homepage.php
@@ -24,25 +24,29 @@ AppAsset::register($this);
 <div class="full-screen">
     <div class="bg-full-screen"></div>
     <header class="transparent-bg">
-        <div class="clearfix">
-            <div class="pull-left">
-                <nav id="main-nav">
-                    <ul class="clearfix">
-                        <li><a href="<?= Url::toRoute('home/') ?>">Trang chủ</a></li>
-                        <li><a href="<?= Url::toRoute('danh-sach-san/') ?>">Danh sách sân</a></li>
-                        <li><a href="<?= Url::toRoute('tim-doi/') ?>">Tìm đối</a></li>
-                        <li><a href="<?= Url::toRoute('ve-chung-toi/') ?>">Về chúng tôi</a></li>
-                    </ul>
-                </nav>
-            </div>
-            <div class="center-logo">
-                <a href="#"><img src="<?= Yii::getAlias('@web')?>/img/logo.png" alt=""></a>
-            </div>
-            <div class="pull-right">
-                <ul id="right-nav" class="clearfix">
-                    <li><a href="<?= Url::toRoute('dang-ky/') ?>">Đăng ký</a></li>
-                    <li><a href="<?= Url::toRoute('dang-nhap/') ?>">Đăng nhập</a></li>
-                </ul>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12 clearfix">
+                    <div class="pull-left">
+                        <nav id="main-nav">
+                            <ul class="clearfix">
+                                <li><a href="<?= Url::toRoute('home/') ?>">Trang chủ</a></li>
+                                <li><a href="<?= Url::toRoute('danh-sach-san/') ?>">Danh sách sân</a></li>
+                                <li><a href="<?= Url::toRoute('tim-doi/') ?>">Tìm đối</a></li>
+                                <li><a href="<?= Url::toRoute('ve-chung-toi/') ?>">Về chúng tôi</a></li>
+                            </ul>
+                        </nav>
+                    </div>
+                    <div class="center-logo">
+                        <a href="#"><img src="<?= Yii::getAlias('@web')?>/img/logo.png" alt=""></a>
+                    </div>
+                    <div class="pull-right">
+                        <ul id="right-nav" class="clearfix">
+                            <li><a href="<?= Url::toRoute('dang-ky/') ?>">Đăng ký</a></li>
+                            <li><a href="<?= Url::toRoute('dang-nhap/') ?>">Đăng nhập</a></li>
+                        </ul>
+                    </div>
+                </div>
             </div>
         </div>
     </header>
