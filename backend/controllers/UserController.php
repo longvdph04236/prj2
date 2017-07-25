460ca0a63c1192097e20d808e4dea2ef02490345
hoanv1992 <hoanvph04233@fpt.edu.vn>
2017-07-25 17:51:37 +0700

    update backend

---
+++ backend/controllers/UserController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace backend\controllers;
+
+use yii;
+use yii\web\Controller;
+use app\models\Users;
+use yii\data\Pagination;
+
+
+
+class UserController extends Controller
+{
+    public function actionIndex()
+    {
+        $this->view->blocks['content-header'] = 'adb';
+        $model = new Users();
+        $query = $model->getCount();
+        $count = $query->count();
+        $pagination = new Pagination(['totalCount'=>$count]);
+        $pagination->setPageSize(5);
+        $articles = $query->offset($pagination->offset)
+            ->limit($pagination->limit)
+            ->all();
+        return $this->render('index',['model'=>$articles,'pagination'=>$pagination]);
+    }
+
+}
---
+++ backend/views/user/index.php
@@ -0,0 +1,93 @@
+<?php
+
+     use app\models\Users;
+     use yii\widgets\LinkPager;
+     use yii\widgets\Breadcrumbs;
+     use yii\helpers\Html;
+
+     $this->title = 'User';
+     $this->params['breadcrumbs'][] = $this->title;
+     Breadcrumbs::widget([
+        'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
+     ]);
+?>
+     <div class="panel panel-primary">
+           <div class="panel-heading">
+            <h4><?= Html::encode($this->title) ?></h4>
+           </div>
+           <div class="panel-body">
+               <table class="table">
+                   <thead>
+                        <th>Avatar</th>
+                        <th>ID</th>
+                        <th>User Name</th>
+                        <th>Email</th>
+                        <th>Phone</th>
+                        <th>Status</th>
+                   </thead>
+                   <tbody>
+                        <?php
+                            foreach($model as $key=>$value) { ?>
+                                <tr>
+                                    <td>
+                                        <?php
+                                            if($value->avatar == '') {
+                                                echo Html::img(Yii::$app->params['appFolder'].'/uploads/images/img_avatar3.png',
+                                                    [  'style' => [
+                                                            'width'=>'60px',
+                                                            'height'=>'60px',
+                                                            'border-radius'=>'50%'
+                                                        ]
+                                                    ]
+                                                );
+                                            }else {
+                                                echo Html::img(Yii::$app->params['appFolder'].'/uploads/images/img_avatar3.png',
+                                                    [
+                                                        'style' => [
+                                                            'width'=>'60px',
+                                                            'height'=>'60px',
+                                                            'border-radius'=>'50%'
+                                                        ]
+                                                    ]
+                                                    );
+                                            }
+                                        ?>
+                                    </td>
+
+                                    <td>
+                                        <?= $value->id ?>
+                                    </td>
+
+                                    <td>
+                                        <?= $value->username ?>
+                                    </td>
+
+                                    <td>
+                                        <?= $value->email ?>
+                                    </td>
+
+                                    <td>
+                                        <?= $value->phone ?>
+                                    </td>
+
+                                    <td>
+                                        <?= $value->status ?>
+                                    </td>
+                                    <td>
+                                        <?= html::a('<span class="glyphicon glyphicon-eye-open"></span>',['view','id'=>$value->id])?>
+                                        <?= html::a('<span class="glyphicon glyphicon-pencil"></span>',['update','id'=>$value->id])?>
+                                        <?= html::a('<span class="glyphicon glyphicon-trash"></span>',['delete','id'=>$value->id],[
+                                            'data'=>[
+                                                'confirm'=>'bạn có chắc chắn muốn xóa : ' .$value->username,
+                                                'method'=>'post'
+                                            ]
+                                        ])?>
+                                    </td>
+                                </tr>
+                        <?php } ?>
+                   </tbody>
+               </table>
+           </div>
+     </div>
+
+
